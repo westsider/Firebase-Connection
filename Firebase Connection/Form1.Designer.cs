4ee0cae722a6ae87ecef1567f23d664593b622b5
Warren Hansen <whansen1@mac.com>
2017-09-19 20:57:08 -0700

    initial commit

--- Firebase Connection/Form1.Designer.cs
+++ Firebase Connection/Form1.Designer.cs
@@ -28,12 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(162, 63);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(5);
+            this.label1.Size = new System.Drawing.Size(309, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Firebase Link is Active";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(612, 168);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Firebase Link";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Firebase Connection/Form1.cs
+++ Firebase Connection/Form1.cs
@@ -7,14 +7,106 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+
 
 namespace Firebase_Connection
 {
+
     public partial class Form1 : Form
     {
+        public int counter;
+        FileSystemWatcher watcher = new FileSystemWatcher();
         public Form1()
         {
             InitializeComponent();
+            // CreateJsonFromCSV();
+            try
+            {
+                string dirName = @"C:\Users\MBPtrader\Documents\FireBase";
+                CreateFileWatcher(dirName);
+                //CreateJsonFromCSV();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "File Import Error");
+                Console.WriteLine("Error ", ex);
+            }
+            //[ ] if file  changed:
+            //[ ]   convert file to json
+            //[ ]   send to firebase
+            //[ ]   delete prior fire base file? decide on program flow
+        }
+
+        
+        //   File watcher magic
+        public void CreateFileWatcher(string path)
+        {
+            
+            watcher.Path = path;
+            watcher.NotifyFilter =  NotifyFilters.LastWrite;
+            watcher.Filter = "*.*";
+            // Add event handlers.
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            //watcher.Created += new FileSystemEventHandler(OnChanged);
+            //watcher.Deleted += new FileSystemEventHandler(OnChanged);
+            // Begin watching.
+            watcher.EnableRaisingEvents = true; 
+        }
+
+        private void OnChanged(object source, FileSystemEventArgs e)
+        {
+            watcher.EnableRaisingEvents = false;
+            counter = counter + 1;
+            Console.WriteLine("File has changed " + counter + " times");
+            //MessageBox.Show("File Change");
+            CreateJsonFromCSV();
+            System.Threading.Thread.Sleep(2000);
+            watcher.EnableRaisingEvents = true;
+        }
+
+        public static void CreateJsonFromCSV()
+        {
+
+            // Not reading csv file
+
+            string path = @"C:\Users\MBPtrader\Documents\FireBase\PriceData.csv";
+            //Read the csv file, and then use System.IO.File.ReadAllLines to read the JSON String format for each line 
+
+            System.Threading.Thread.Sleep(1000);
+
+            // remove Duplicates
+            var sr = new StreamReader(File.OpenRead(path));
+            var sw = new StreamWriter(File.OpenWrite(@"C:\Users\MBPtrader\Documents\FireBase\PriceData_Out.csv"));
+            var lines = new HashSet<int>();
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                int hc = line.GetHashCode();
+                if (lines.Contains(hc))
+                    continue;
+
+                lines.Add(hc);
+                sw.WriteLine(line);
+            }
+            sw.Flush();
+            sw.Close();
+            sr.Close();
+
+            //MARK: - TODO Consider sorting Date so its consecutive on mutiple loads
+
+            //MARK: - TODO Convert to JSON
+
+            //MARK: - TODO Upload to Firebase
+
+        }
+
+        private async void theTimer()
+        {
+            Console.WriteLine("Timer Start");
+            await Task.Delay(3000);
+            Console.WriteLine("Timer End");
+
         }
     }
 }
